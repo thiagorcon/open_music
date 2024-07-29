559116f04ceb3b214627dd7046fa3b8947a667be
Thiago Rosa <thiagorcon@hotmail.com>
2024-07-29 19:04:34 -0300

    Tarefa 2

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,14 @@
- export '../'
- 
- let albunList = document.querySelector('.containet_albunsList');
- 
-let albunListagem.map
+
+
+document.addEventListener("DOMContentLoaded", () => {
+     const genreButtons = document.querySelectorAll(".genre-button");
+   
+     genreButtons.forEach(button => {
+       button.addEventListener("click", () => {
+         genreButtons.forEach(btn => btn.classList.remove("active"));
+         button.classList.add("active");
+       });
+     });
+   });
+   
 
